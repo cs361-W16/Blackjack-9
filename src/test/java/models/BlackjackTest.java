dd7d15e93759cd872a003871afe575a1a3f9e72e
Paul Lantow <paul.lantow@gmail.com>
2016-03-13 22:46:07 -0700

    Added Blackjack tests

---
+++ src/test/java/models/BlackjackTest.java
@@ -0,0 +1,66 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by paullantow on 3/13/16.
+ */
+public class BlackjackTest {
+
+    @Test
+    public void testClearBoard() throws Exception {
+
+    }
+
+    @Test
+    public void testBuildDeck() throws Exception {
+
+    }
+
+    @Test
+    public void testShuffle() throws Exception {
+
+    }
+
+    @Test
+    public void testStartHand() throws Exception {
+
+    }
+
+    @Test
+    public void testPlayerHit() throws Exception {
+
+    }
+
+    @Test
+    public void testDealerHit() throws Exception {
+
+    }
+
+    @Test
+    public void testDPlay() throws Exception {
+
+    }
+
+    @Test
+    public void testScoring() throws Exception {
+
+    }
+
+    @Test
+    public void testCustomDeal() throws Exception {
+
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+}
