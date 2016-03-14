1b9bc6a97a6e79f95b90092e1904f2292261fc8e
Paul Lantow <paul.lantow@gmail.com>
2016-03-13 22:36:56 -0700

    Double Down test

--- src/test/java/models/testUser.java
+++ src/test/java/models/testUser.java
@@ -4,7 +4,8 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * Created by Dilon_000 on 3/13/2016.
@@ -20,6 +21,19 @@ public class testUser {
         person.userHit(deck, row);
         assertNull(deck.get(0));
         assertEquals(row.get(0).suit, Suit.Hearts);
+    }
+
+    @Test
+    public void doubleDown() {
+        User person = new User();
+        Dealer dealer = new Dealer();
+        java.util.List<Card> deck = new ArrayList<>();
+        java.util.List<Card> userRow = new ArrayList<>();
+        java.util.List<Card> dealerRow = new ArrayList<>();
+        deck.add(new Card(0, Suit.Hearts));
+        person.doubleDown(deck, userRow, dealer, dealerRow);
+        assertNull(deck.get(0));
+        assertEquals(userRow.get(0).suit, Suit.Hearts);
     }
 
 
