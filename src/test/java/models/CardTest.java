4d0a9817800aa52112be7ba793c789f785088abe
Paul Lantow <paul.lantow@gmail.com>
2016-03-13 22:59:33 -0700

    100% test coverage for card.

--- src/test/java/models/CardTest.java
+++ src/test/java/models/CardTest.java
@@ -11,16 +11,19 @@ public class CardTest {
 
     @Test
     public void testGetSuit() throws Exception {
-
+        Card card = new Card(1,Suit.Spades);
+        assertEquals(card.getSuit(),Suit.Spades);
     }
 
     @Test
     public void testGetValue() throws Exception {
-
+        Card card = new Card(4,Suit.Spades);
+        assertEquals(card.getValue(),4);
     }
 
     @Test
     public void testToString() throws Exception {
-
+        Card card = new Card(10,Suit.Clubs);
+        assertEquals(card.toString(),10+Suit.Clubs.toString());
     }
 }
